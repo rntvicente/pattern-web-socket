e78edaa89cf4164095d9b3b2de877e675e4c3d89
renato.vicente <rntvicente@gmail.com>
2018-08-14 21:40:31 -0300

    encerramente da lição de socket-io

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const socketio = require('socket.io');
 const app = require('./app/server');
 const config = require('./app/commons/config');
 
+const emit = (socket, data, event) => {
+  socket.emit(event, {
+    apelido: data.apelido,
+    message: data.message
+  });
+};
+
+const broadcast = (socket, data, event) => {
+  socket.broadcast.emit(event, {
+    apelido: data.apelido,
+    message: data.message
+  });
+};
+
 const server = app.listen(config.get('PORT'), (err) => {
   if (err) {
     console.error(chalk.red(`Error on listen port ${err.message}`));
@@ -24,14 +38,14 @@ io.on('connection', (socket) => {
   });
 
   socket.on('message-to-server', (data) => {
-    socket.emit('message-to-custumer', {
-      apelido: data.apelido,
-      message: data.message
-    });
+    /*dialog*/
+    emit(socket, data, 'message-to-custumer');
+    broadcast(socket, data, 'message-to-custumer');
 
-    socket.broadcast.emit('message-to-custumer', {
-      apelido: data.apelido,
-      message: data.message
-    });
+    /*participants*/
+    if (parseInt(data.apelidoCheck) === 0) {
+      emit(socket, data, 'participants-chat');
+      broadcast(socket, data, 'participants-chat');
+    }
   });
 });
